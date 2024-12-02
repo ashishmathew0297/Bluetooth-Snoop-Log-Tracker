6a7d52b6f42bbebb2fa6ae1adc4f666eb29b008b
Ashish Mathew <ashishmathew0297@gmail.com>
2024-12-02 15:58:15 -0500

    Code in progress. Added pie charts

--- src/bug_report_generator.sh
+++ src/bug_report_generator.sh
@@ -22,7 +22,6 @@ tshark -r ./bt_logfiles/btsnoop_hci_10032024_1156.log -T json \
 -e frame.time_utc \
 -e frame.len \
 -e frame.protocols \
--e hci_h4 \
 -e hci_h4.type \
 -e hci_h4.direction \
 -e bthci_cmd \
@@ -40,11 +39,10 @@ tshark -r ./bt_logfiles/btsnoop_hci_10032024_1156.log -T json \
 -e bthci_acl.dst.bd_addr \
 -e bthci_acl.dst.name \
 -e bthci_acl.dst.role \
--e btl2cap.command \
--e btl2cap.cmd_length \
+-e btl2cap.cid \
 -e btl2cap.length \
 -e btatt.opcode \
--e btatt.value > output1.json
+-e btatt.value > ./output1.json
 
 printf "%s%sCompleted%s\n" "${BOLD}" "${GREEN}" "${NORMAL}"
 
